1610e601631e44e33fd86b21ae265f00685ed175
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2022-08-25 15:36:05 +0200

    finish game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Guess the number!");
+
+    loop {
+        println!("Type your guess:");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You guessed correctly!");
+                break;
+            },
+            Ordering::Greater => println!("Too big!")
+        }
+    }
+
 }
